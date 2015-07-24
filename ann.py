179ed3db243b2a8ee75fb66b4253efc5f83fa837
Viktor Wase <ViktorWase@users.noreply.github.com>
2015-07-24 20:22:56 +0200

    Changed the sigmoid function.
    This should probably give faster convergence. Maybe.

--- ann.py
+++ ann.py
@@ -1,8 +1,11 @@
 from random import random
 from math import fabs
 
-def sigmoidFunc(x):
+def sigmoidFunc2(x):
         return x/(1.0+fabs(x))
+
+def sigmoidFunc(x):
+        return 1.0/(1.0+exp(-x))-0.5
 
 def artificialNeuralNetwork(obj, weights):
     """
