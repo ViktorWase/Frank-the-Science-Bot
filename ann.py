d282026dc73b557b9c604f678203242ffef009f6
Viktor Wase <viktorwase@gmail.com>
2015-08-02 11:24:22 +0200

    Fixed the ANNs
    Made sure that the ANN can have any shape.
    It should be noted that once an ANN is generated, its shape is never
    changed. Only its weights.

--- ann.py
+++ ann.py
@@ -1,35 +1,66 @@
 from random import random
 from math import fabs
 
+import scienceFunctions
+
 def sigmoidFunc2(x):
         return x/(1.0+fabs(x))
 
 def sigmoidFunc(x):
         return 1.0/(1.0+exp(-x))-0.5
 
+def isCorrectLength(obj,weights):
+    try:
+        n_o = len(obj)
+        n_w = len(weights)
+
+        if(n_o<=0 or n_w <= 0):
+            return False
+
+        hiddenLayers = weights[1:(weights[0]+1)]
+        if(len(hiddenLayers) != weights[0]):
+            print "fuck"
+
+        counter = len(hiddenLayers) + 1
+        counter += (n_o + 1)*hiddenLayers[0]
+        for i in range(len(hiddenLayers)-1):
+            counter += hiddenLayers[i]*hiddenLayers[i+1]
+        counter += hiddenLayers[len(hiddenLayers)-1]
+
+        if(n_w != counter):
+            return False
+
+    except:
+        return False
+
+    return True
+
+
 def artificialNeuralNetwork(obj, weights):
     """
         obj is a list of values (like [0.5, 6.0, 0]. Keep away
         from inf and nan and stuff).
-        weights is a list of values as well. It should be of size
-        220+10*len(obj). Dunno why.
+
+        weights is a vector where the first element is the number
+        of hidden layers. (Call it N)
+        Then there should follow N integers that show how many nodes
+        there should be in the N hidden layers.
+        Then there are a fuckbunch of floats that symbolize the actual
+        parameters (or weights) of the neural network.
     """
     oldLayer = list(obj.attributes)
 
-    #These are ad hoc as fuck and should probably be fixed.
-    hiddenLayers = 3
-    nodesInHiddenLayers = [10, 10, 10]
+    #Prepares the stuff for the network.
+    hiddenLayers = weights[0]
+    counter = 1
+    nodesInHiddenLayers = []
+    for i in range(hiddenLayers):
+        nodesInHiddenLayers.append(weights[counter])
+        counter += 1
 
     #Always add a one to avoid bias
     oldLayer.append(1.0)
 
-    #Sometimes add a random to instil fear in the hearts of your enemies
-    #But not today.
-    isRandom = False
-    if(isRandom):
-        oldLayer.append(2.0*random()-1.0)
-
-    counter = 0
     for i in range(hiddenLayers):
         layer = [0.0]*nodesInHiddenLayers[i]
         for j in range(len(layer)):
@@ -44,6 +75,6 @@ def artificialNeuralNetwork(obj, weights):
     for i in range(nodesInHiddenLayers[hiddenLayers-1]):
         out += layer[i]*weights[counter]
         counter += 1
-    print counter
+    #print counter
 
     return out
--- scienceFunctions.py
+++ scienceFunctions.py
@@ -1,5 +1,31 @@
 import random
 
+def howManyWeightsInTheANN(hiddenLayers, numHiddenLayers, numInputs):
+    """
+        This calculates how many weights the ANN needs in order
+        to function.
+    """
+    counter = (numInputs + 1)*hiddenLayers[0]
+    for i in range(len(hiddenLayers)-1):
+        counter += hiddenLayers[i]*hiddenLayers[i+1]
+    counter += hiddenLayers[len(hiddenLayers)-1]
+    return counter
+
+def georand(p):
+    """
+        This is the geometric distribution with
+        parameter p. They didn't have it in the
+        Random library. Not sure why.
+    """
+    if(p<0 or p>1.0):
+        print "p should be between 0 and 1 (georand)"
+        return
+
+    counter = 1
+    while(random.random() <= p):
+        counter += 1
+    return counter
+
 def someNorm(vector):
     """
         Lets go with the ordinary 2-norm.
@@ -16,11 +42,32 @@ def randomHypothesis(n):
         The idea is that this should return a random
         function. But a random ANN is the next best
         thing.
+
+        n is the number of inputs of the function.
     """
-    out = [0.0]*(220+10*n)
-    for index in range(len(out)):
-        if(random.random()<0.1):
+
+    #Generate the hidden layers!
+    #The number of hidden layers is geometrically random
+    numHiddenLayers = georand(0.8)
+    hiddenLayers = [0]*numHiddenLayers
+    #The number of nodes in each layer is geometrically random as well
+    for i in range(numHiddenLayers):
+        hiddenLayers[i] = georand(0.95)
+
+    #How many weights are required in the ANN?
+    numWeights = howManyWeightsInTheANN(hiddenLayers, numHiddenLayers, n)
+
+    out = [0.0]*(numWeights+numHiddenLayers+1)
+    out[0] = numHiddenLayers
+    index = 1
+    for element in hiddenLayers:
+        out[index] = element
+        index += 1
+
+    for i in range(len(out)):
+        if(random.random()<0.2):
             out[index] = 10.0*random.gauss(0.0,1.0)
+        index += 1
     return out
 
 def addRandomLittleJump(w):
@@ -28,8 +75,12 @@ def addRandomLittleJump(w):
         Adds a small and random change to the
         weight vector.
     """
-    for index in range(len(w)):
+    out = list(w)
+
+    counter = 1 + w[0]
+
+    for index in range(len(w)-counter):
         #Try to keep the hypothesis sparse.
-        if(w[index] != 0 or random.random() < 0.05):
-            w[index] = w[index] + random.gauss(0.0, 0.05)
-    return w
+        if(w[index+counter] != 0 or random.random() < 0.05):
+            out[index+counter] += random.gauss(0.0, 0.05)
+    return out
